dfd1766121d62af04f1400893a4c91a261167ad9
Per Øyvind Karlsen <proyvind@moondrake.org>
2018-04-11 21:59:42 +0200

    include bindings generated by bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+
 #[link(name = "rpm")]
+#[link(name = "rpmbuild")]
+#[link(name = "rpmio")]
 
 #[cfg(test)]
 mod tests {
